98a791459390e2e2fb191c7519ffd12a3532f18e
Ben Mukebo <l.benkasmukebo7@gmail.com>
2021-12-07 21:20:29 +0200

    Create patients and treatments tables

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,3 +1,14 @@
 /* Database schema to keep the structure of entire database. */
 CREATE DATABASE clinic;
 
+CREATE TABLE patients (
+	id BIGSERIAL NOT NULL PRIMARY KEY,
+	name VARCHAR(100) NOT NULL,
+	date_of_birth DATE NOT NULL,
+);
+
+CREATE TABLE treatments (
+	id BIGSERIAL NOT NULL PRIMARY KEY,
+	type VARCHAR NOT NULL,
+	name VARCHAR(100) NOT NULL,
+);
