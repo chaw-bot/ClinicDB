56c3100ff34633b7c14acf30df13b7bce1a895e1
chaw <77142475+chaw-bot@users.noreply.github.com>
2021-12-07 21:42:03 +0200

    create medical_histories table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,14 +1,22 @@
 /* Database schema to keep the structure of entire database. */
-CREATE DATABASE clinic;
+-- CREATE DATABASE clinic;
 
 CREATE TABLE patients (
 	id BIGSERIAL NOT NULL PRIMARY KEY,
-	name VARCHAR(100) NOT NULL,
-	date_of_birth DATE NOT NULL,
+	name VARCHAR(250) NOT NULL,
+	date_of_birth DATE NOT NULL
 );
 
 CREATE TABLE treatments (
 	id BIGSERIAL NOT NULL PRIMARY KEY,
 	type VARCHAR NOT NULL,
-	name VARCHAR(100) NOT NULL,
+	name VARCHAR(250) NOT NULL
 );
+
+CREATE TABLE medical_histories (
+	id BIGSERIAL NOT NULL PRIMARY KEY,
+	admitted_at TIMESTAMP,
+	patient_id INT,
+	status VARCHAR(250)
+);
+
